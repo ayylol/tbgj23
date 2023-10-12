b5f91f804f635ab1a4d15ccd257f390c2ad0998c
David Garcia <David.GarOro@protonmail.com>
2023-10-11 19:19:45 -0600

    Some changes lol

--- src/main.c
+++ src/main.c
@@ -1,34 +1,55 @@
 #include <ncurses.h>
-#include <unistd.h>
 
 struct Vec2 {
-    int x;
-    int y;
+    int32_t x;
+    int32_t y;
 };
-
-struct Actor {
-    struct Vec2 pos;
-};
-
-struct Vec2 add_vec2(struct Vec2 a, struct Vec2 b) {
+struct Vec2 
+add_vec2(struct Vec2 a, struct Vec2 b) {
     return (struct Vec2) {
         .x = a.x + b.x, 
         .y = a.y + b.y };
 }
-struct Vec2 actor_move(struct Actor *a, struct Vec2 move);
 
-int main(int argc, char *argv[]){
-    int ch=0;
+struct Actor {
+    char character;
+    struct Vec2 pos;
+};
+struct Vec2 
+actor_move(struct Actor *a, struct Vec2 move){
+    struct Vec2 pos_after = add_vec2(a->pos, move);
+    // Check collision
+    if ( true ){
+        a->pos = pos_after;
+    }
+    return a->pos;
+}
+enum Attribute {
+    ATTR_CAN_PLACE  = 0b0001,
+    ATTR_IS_HOUSE   = 0b0001,
+    ATTR_OBSTACLE   = 0b0010,
+    ATTR_HAS_SIGN   = 0b0100,
+    ATTR_INFLUENCE  = 0b1000,
+};
+enum Tile {
+    T_ROAD          = 0b00,
+    T_GRASS         = ATTR_CAN_PLACE,
+    T_OBSTACLE      = ATTR_OBSTACLE,
+    T_HOUSE         = ATTR_OBSTACLE | ATTR_IS_HOUSE
+};
+
+int 
+main(){
+    uint32_t ch=0;
     initscr();
     cbreak();
     keypad(stdscr, true);
     noecho();
     curs_set(0);
 
-    struct Actor player={.pos = {.x=0,.y=0}};
-    while( ch != 'q')
-    {
-        mvaddch(player.pos.y, player.pos.x,'A');
+    struct Actor player={.character = '&', .pos = {.x=0,.y=0}};
+    while( ch != 'q' ) {
+        mvaddch(player.pos.y, player.pos.x,player.character|A_BOLD);
         refresh();
         ch = getch();
         struct Vec2 move = {0,0};
@@ -43,11 +64,3 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-struct Vec2 actor_move(struct Actor *a, struct Vec2 move){
-    struct Vec2 pos_after = add_vec2(a->pos, move);
-    // Check collision
-    if ( true ){
-        a->pos = pos_after;
-    }
-    return a->pos;
-}
